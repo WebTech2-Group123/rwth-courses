93917955180f48c6b8fafd0366efdd783f1d021a
Davide Pedranz <davide.pedranz@gmail.com>
2016-01-17 13:57:34 +0100

    Refactor Mongo

--- src/mongo.js
+++ src/mongo.js
@@ -1,8 +1,17 @@
 'use strict';
 
+// mongo
 var MongoClient = require('mongodb').MongoClient;
+var Promise = require("bluebird");
+Promise.promisifyAll(require("mongodb"));
+
+// logs
 var log = require('debug')('db');
 var assert = require('assert');
+
+// constants
+const COURSES = 'courses';
+const COURSES_TEMP = 'courses_temp';
 
 // constructor
 var Mongo = function (url) {
@@ -16,21 +25,43 @@ Mongo.prototype.connect = function () {
     MongoClient.connect(this.url, (err, db) => {
         assert.equal(null, err);
         log('Connected to MongoDB');
+
+        // save db instance
         this.db = db;
+
+        // create courses collection
+        db.createCollectionAsync(COURSES).then(collection => {
+            this.courses = collection;
+        });
+
+        // create courses_temp collection
+        db.createCollectionAsync(COURSES_TEMP).then(collection => {
+            this.coursesTemp = collection;
+        });
     });
 
-    // disconnect on programm exit
-    process.on('SIGINT', () => {
+    // function to run on node.js programm exit
+    var close = () => {
         this.db.close();
         log('Disconnected from MongoDB');
+    };
+
+    // disconnect on programm exit
+    process.on('exit', close);
+    process.on('SIGINT', close);
+};
+
+// drop temp_courses collection
+Mongo.prototype.renameTempCourses = function () {
+    tempCourses.rename(COURSES_TEMP, {dropTarget: true}, (err, _) => {
+        assert.equal(null, err);
+        log('Rename ' + COURSES_TEMP + ' to ' + COURSES);
     });
 };
 
-
-
 // export
 exports.Mongo = Mongo;
-exports.createClient = function(url) {
+exports.createClient = function (url) {
     var db = new Mongo(url);
     db.connect();
     return db;
--- src/test.js
+++ src/test.js
@@ -1,3 +1,7 @@
+'use strict';
+
 var db = require('./mongo').createClient();
 
 var a = 1;
+
+setTimeout(process.exit, 3000);
