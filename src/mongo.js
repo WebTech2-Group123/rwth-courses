0ca64fe9f6fa628695146c3d6cc6c327edb8391b
Davide Pedranz <davide.pedranz@gmail.com>
2016-01-17 12:25:57 +0100

    Refactor mongo

---
+++ src/mongo.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var MongoClient = require('mongodb').MongoClient;
+var log = require('debug')('db');
+var assert = require('assert');
+
+// constructor
+var Mongo = function (url) {
+    this.url = url || process.env.MONGODB_URL || 'mongodb://localhost:27017/rwth-courses';
+};
+
+// insert course
+Mongo.prototype.connect = function () {
+
+    // use connect method to connect to the Server
+    MongoClient.connect(this.url, (err, db) => {
+        assert.equal(null, err);
+        log('Connected to MongoDB');
+        this.db = db;
+    });
+
+    // disconnect on programm exit
+    process.on('SIGINT', () => {
+        this.db.close();
+        log('Disconnected from MongoDB');
+    });
+};
+
+// export
+exports.Mongo = Mongo;
+exports.createClient = function(url) {
+    var db = new Mongo(url);
+    db.connect();
+    return db;
+};
---
+++ src/test.js
@@ -0,0 +1,3 @@
+var db = require('./mongo').createClient();
+
+var a = 1;
