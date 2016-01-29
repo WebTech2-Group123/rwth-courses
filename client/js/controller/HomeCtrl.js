5bfab2f8962c81d68c67e1398a953bd494f48af3
Alexander Schmidt <alexander.schmidt1@rwth-aachen.de>
2016-01-29 16:59:47 +0100

    Container, tabs and nice semester choice

--- client/js/controller/HomeCtrl.js
+++ client/js/controller/HomeCtrl.js
@@ -2,7 +2,19 @@ app.controller('HomeCtrl', HomeCtrl);
 
 function HomeCtrl($scope, $filter, $location, $http) {
 
-    $scope.semesters = ["WS 2015/2016", "SS 2015"];
+    // TODO: get semesters from the backend
+    var semesters = ["WS 2015/2016", "SS 2015"];
+
+    // show the latest semester by default
+    $scope.currentSemester = semesters[0];
+
+    $scope.changeSemester = function () {
+        if($scope.currentSemester == semesters[0]){
+            $scope.currentSemester = semesters[1];
+        }else{
+            $scope.currentSemester = semesters[0];
+        }
+    }
 
     $scope.fields = ["Architektur (D)", "Bauingenieurwesen (D)", "Metallurgie und Werkstofftechnik (D)", "Werkstoffinformatik (D)", "Wirtschaftsingenieurwesen FR Bauingenieurwesen (D)", "Technik-Kommunikation (M.A.)", "Erziehungswissenschaftliches Studium (GYM+GS,BK,SII)", "Faszination Technik (GYM+GS,BK)", "Neue Medien (GYM+GS,BK,SII)", "Bautechnik (BK,SII)", "Biologie (GYM+GS,BK,SII)", "Chemie (GYM+GS,BK,SII)", "Deutsch (GYM+GS,BK,SII)", "Elektrotechnik (BK,SII)", "Energietechnik (BK,SII)", "Englisch (GYM+GS,BK,SII)", "Evangelische Religionslehre (SII)", "Fahrzeugtechnik (BK,SII)", "Fertigungstechnik (BK,SII)", "Französisch (GYM+GS,BK,SII)"];
 
