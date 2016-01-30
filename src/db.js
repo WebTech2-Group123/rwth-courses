fe723c980dedd6021411836147521e8f7843d105
Davide Pedranz <davide.pedranz@gmail.com>
2016-01-30 01:53:51 +0100

    Move to Mongoose

---
+++ src/db.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var log = require('debug')('db');
+var mongoose = require('mongoose');
+
+// constructor
+var DB = function (url) {
+
+    // choose best db
+    this.url = url || process.env.MONGO_URL || 'mongodb://localhost/rwth-courses';
+
+    // connect to db
+    this.connection = mongoose.createConnection(this.url);
+    this.connection.on('open', () => {
+        log('Connected to ' + this.url);
+    });
+    this.connection.on('close', () => {
+        log('Disconnected to ' + this.url);
+    });
+
+    // Cache Schema
+    var CacheSchema = new mongoose.Schema({
+        gguid: {
+            type: String,
+            required: true,
+            index: true
+        }
+    }, {strict: false});
+
+    // Caches
+    this.SemestersCache = this.connection.model('SemestersCache', CacheSchema);
+    this.FieldsCache = this.connection.model('FieldsCache', CacheSchema);
+    this.SubFieldsCache = this.connection.model('SubFieldsCache', CacheSchema);
+    this.CoursesListCache = this.connection.model('CoursesListCache', CacheSchema);
+    this.CoursesDetailsCache = this.connection.model('CoursesDetailsCache', CacheSchema);
+
+    // Courses Schema
+    var CoursesSchema = new mongoose.Schema({
+        gguid: {
+            type: String,
+            required: true,
+            index: true
+        }
+    }, {strict: false});
+
+    // Courses
+    this.Courses = this.connection.model('Courses', CoursesSchema);
+    this.CoursesTemp = this.connection.model('CoursesTemp', CoursesSchema);
+};
+
+DB.prototype.close = function () {
+    this.connection.close();
+};
+
+// Singleton
+var instance = null;
+function getInstance() {
+    if (instance === null) {
+        instance = new DB();
+    }
+    return instance;
+}
+
+// exports
+exports.DB = DB;
+exports.getInstance = getInstance;
+
+
+// TODO -> remove
+
+var db1 = new DB();
+var db2 = new DB('mongodb://localhost/rwth');
+
+db1.close();
+
+setTimeout(() => db2.close(), 2000);
