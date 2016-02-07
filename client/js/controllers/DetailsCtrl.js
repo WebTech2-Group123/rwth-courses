82c288540f52711c299aba4314f09b2471e3145d
Alexander Schmidt <alexander.schmidt1@rwth-aachen.de>
2016-02-07 23:22:08 +0100

    Start with details page

--- client/js/controllers/DetailsCtrl.js
+++ client/js/controllers/DetailsCtrl.js
@@ -1,7 +1,41 @@
-app.controller('DetailsCtrl', function($scope, $routeParams, $location, localStorageService){
+app.controller('DetailsCtrl', function ($scope, $routeParams, $location, localStorageService, Courses) {
     $scope.gguid = $routeParams.gguid;
 
-    //console.log(localStorageService.get('lastPath'));
+    function compareString(str1, str2) {
+        return str1.indexOf(str2);
+    }
+
+    function testEmpty(item) {
+        return item.length > 0 ? true : false;
+    }
+
+    Courses.getByIDs($scope.gguid).then(function (courses) {
+
+        // take the course
+        $scope.course = courses.pop();
+
+        // show german name if it differs from english one
+        $scope.showNameDe = function () {
+            return compareString($scope.course.name, $scope.course.name_de);
+        }
+
+        // show description in german if available
+        $scope.showDescriptionDe = function () {
+            return compareString($scope.course.description, $scope.course.description_de);
+        }
+
+        // show item if available
+        $scope.showItem = function (item) {
+            return testEmpty(item);
+        }
+
+        // show box if at least one item is available
+        $scope.showBox = function (items) {
+            return items.map(function (item) {
+                return testEmpty(item);
+            }).indexOf(true) != -1;
+        }
+    });
 
     $scope.closeDetails = function () {
         $location.url(localStorageService.get('lastPath'));
--- client/js/filter/languagesFilter.js
+++ client/js/filter/languagesFilter.js
@@ -1,6 +1,7 @@
 // limit to max len
 app.filter('languages', function ($sce) {
     return function (input) {
+        var input = input || [];
         var html = input.map(function (language) {
             return '<img src="img/flags/' + language.toLowerCase() + '.png" width="30" alt="flag">'
         }).join('&nbsp;&nbsp;&nbsp;');
