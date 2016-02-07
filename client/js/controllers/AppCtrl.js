ad697e7d8268d76fa587c76e2109deea8c7e47ac
Alexander Schmidt <alexander.schmidt1@rwth-aachen.de>
2016-02-07 13:36:48 +0100

    Go home with RWTH-Logo, clean app.html and appctrl

--- client/js/controllers/AppCtrl.js
+++ client/js/controllers/AppCtrl.js
@@ -1,7 +1,5 @@
 app.controller('AppCtrl', function ($scope, $location, $log, localStorageService) {
 
-    $scope.courseListExist = true;
-
     if (localStorageService.get('selected') && localStorageService.get('selected').length > 0) {
         $scope.selectedCoursesExist = false;
     } else {
@@ -10,73 +8,25 @@ app.controller('AppCtrl', function ($scope, $location, $log, localStorageService
 
     // cache location of course list
     $scope.$on('$locationChangeStart', function (event, newUrl, oldUrl) {
-        //console.log('New URL: ' + newUrl);
-        //console.log('Old URL: ' + oldUrl);
-        //console.log(typeof newUrl);
-        //console.log(typeof oldUrl);
-
-        // enable course list tab
-        if (newUrl) {
-            if (newUrl.indexOf('/courses') != -1) {
-                $scope.courseListExist = false;
-            }
-        }
 
         if (oldUrl) {
             if (oldUrl.lastIndexOf('/courses') != -1) {
                 var index = oldUrl.lastIndexOf('/courses');
                 var path = oldUrl.substr(index, oldUrl.length);
-                //console.log('Path courses: ' + path);
                 localStorageService.set('coursePath', path);
                 localStorageService.set('lastPath', path);
             } else {
                 var path = oldUrl.substr(oldUrl.lastIndexOf('/'), oldUrl.length);
                 localStorageService.set('lastPath', path);
-                //console.log('Path everything else: ' + path);
             }
 
         }
 
-        //console.log('Storage coursePath: ' + localStorageService.get('coursePath'));
-        //console.log('STorage lastPath: ' + localStorageService.get('lastPath'));
-        //
-        //console.log('----------------------');
-
     });
 
-    $scope.$on('$routeChangeStart', function (event, newRoute, oldRoute) {
-        //console.log('Old route: ' + oldRoute);
-        //console.log(oldRoute);
-        //console.log('New route: ' + newRoute);
-        //console.log(newRoute);
-    });
-
-    //$scope.courseActive = function (path) {
-    //
-    //    var  path = path;
-    //
-    //    $scope.$on('$routeChangeStart', function(event, newRoute, oldRoute){
-    //
-    //        console.log(path);
-    //
-    //        var currLocation = '';
-    //
-    //        // get the beginning of the current path
-    //        console.log("Location URL: " + $location.url());
-    //        if ($location.url() != '/null') {
-    //            currLocation = '/' + ($location.url().split('/', 2)).join('');
-    //            console.log("currLocation: " + currLocation);
-    //        } else {
-    //            currLocation = localStorageService.set('coursePath', path);
-    //            $log.error('Location URL is null');
-    //            return;
-    //        }
-    //
-    //        return (path == currLocation);
-    //
-    //    })
-    //
-    //}
+    $scope.goHome = function () {
+        $location.url('/');
+    }
 
     $scope.goToRoute = function (target) {
         switch (target) {
