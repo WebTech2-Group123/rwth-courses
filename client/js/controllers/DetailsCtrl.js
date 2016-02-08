39808d05aed6800338c7845fc46b1a3c7c3c81d9
Davide Pedranz <davide.pedranz@gmail.com>
2016-02-08 19:16:23 +0100

    Replace logo with button£

--- client/js/controllers/DetailsCtrl.js
+++ client/js/controllers/DetailsCtrl.js
@@ -1,6 +1,6 @@
-app.controller('DetailsCtrl', function ($scope, $routeParams, $location, $sce, localStorageService, Courses) {
-    var gguid = $routeParams.gguid;
+app.controller('DetailsCtrl', function ($scope, $routeParams, $location, $sce, localStorageService, Courses, $window) {
 
+    var gguid = $routeParams.gguid;
     $scope.$parent.close = true;
 
     function compareString(str1, str2) {
@@ -8,7 +8,7 @@ app.controller('DetailsCtrl', function ($scope, $routeParams, $location, $sce, l
     }
 
     function testEmpty(item) {
-        return item.length > 0 ? true : false;
+        return item.length > 0;
     }
 
     Courses.getByIDs(gguid).then(function (courses) {
@@ -19,36 +19,33 @@ app.controller('DetailsCtrl', function ($scope, $routeParams, $location, $sce, l
         // show german name if it differs from english one
         $scope.showNameDe = function () {
             return compareString($scope.course.name, $scope.course.name_de);
-        }
+        };
 
         // show description in german if available
         $scope.showDescriptionDe = function () {
             return compareString($scope.course.description, $scope.course.description_de);
-        }
+        };
 
         // show item if available
         $scope.showItem = function (item) {
             return item ? testEmpty(item) : false;
-        }
+        };
 
         // show box if at least one item is available
         $scope.showBox = function (items) {
             return items.map(function (item) {
                     return testEmpty(item);
                 }).indexOf(true) != -1;
-        }
+        };
     });
 
     $scope.$parent.goToCampus = function () {
         const BASE_URL = 'https://www.campus.rwth-aachen.de/rwth/all/event.asp?gguid=';
-
-        console.log(BASE_URL + gguid);
-
-        return $sce.trustAsResourceUrl(BASE_URL + gguid);
-    }
+        $window.open(BASE_URL + gguid, '_blank');
+    };
 
     $scope.$parent.closeDetails = function () {
         $scope.$parent.close = false;
         $location.url(localStorageService.get('lastPath'));
-    }
+    };
 });
